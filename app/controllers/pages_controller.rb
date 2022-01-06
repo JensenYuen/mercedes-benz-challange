eaf351c4db6eb3784c665d430bfaf07b3a6e5804
jensen yuen <jensenyuen@outlook.com>
2022-01-06 16:13:58 +0800

    Added tests to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,15 @@ class PagesController < ApplicationController
   end
 
   def search
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    uri = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=#{longitude},#{latitude}&access_token=#{ENV['MAPBOX_API']}")
-    museums_hash = parse_uri(uri)
-    render json: museums_hash
+    if check_params(params)
+      latitude = params[:latitude]
+      longitude = params[:longitude]
+      uri = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=#{longitude},#{latitude}&access_token=#{ENV['MAPBOX_API']}")
+      museums_hash = parse_uri(uri)
+      render json: museums_hash
+    else
+      redirect_to root_path
+    end
   end
 
   private
@@ -16,12 +20,19 @@ class PagesController < ApplicationController
     museums_hash = {}
     api_response = Net::HTTP.get_response(uri)
     api_json = JSON.parse(api_response.body)
-    museums = api_json['features']
-    museums.each do |museum|
-      postal_code = museum['context'][0]['text'].to_sym
-      museum_name = museum['text']
-      museums_hash[postal_code].present? ? museums_hash[postal_code] << museum_name : museums_hash[postal_code] = [museum_name]
+    if api_json['features'].present?
+      museums = api_json['features']
+      museums.each do |museum|
+        postal_code = museum['context'][0]['text'].to_sym
+        museum_name = museum['text']
+        museums_hash[postal_code].present? ? museums_hash[postal_code] << museum_name : museums_hash[postal_code] = [museum_name]
+      end
     end
     museums_hash
+  end
+
+  def check_params(params)
+    regex = /\d+\.\d+/
+    params[:longitude].match?(regex) && params[:latitude].match?(regex)
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'homepage exist' do
+    get root_path
+    assert_response :success
+  end
+
+  test 'params should include longitude and latitude' do
+    get pages_search_path,
+        params: { latitude: 52.494857, longitude: 13.437641 }
+    assert_response :success
+  end
+
+  test 'params left blank' do
+    get pages_search_path,
+        params: { latitude: '', longitude: '' }
+    assert_redirected_to root_path
+  end
+
+  test 'params include letters' do
+    get pages_search_path,
+        params: { latitude: '12.564', longitude: 'qwec.12e' }
+    assert_redirected_to root_path
+  end
 end
