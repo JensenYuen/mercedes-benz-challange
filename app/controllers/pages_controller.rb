0cd763a28fe1c3939c06dff278629d63542f5662
jensen yuen <jensenyuen@outlook.com>
2022-01-06 13:48:35 +0800

    added routing to search method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search
+    latitude = params[:latitude]
+    longitude = params[:longitude]
+    uri = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=#{longitude},#{latitude}&access_token=#{ENV['MAPBOX_API']}")
+    api_response = Net::HTTP.get_response(uri)
+    respond_to do |format|
+      format.html { render json: api_response.body }
+    end
+    raise
+  end
 end
