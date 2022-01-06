11ea8eb3341a3249ef3846a4963d4b40ab56f15d
jensen yuen <jensenyuen@outlook.com>
2022-01-06 15:20:10 +0800

    search displays json of museums

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,22 @@ class PagesController < ApplicationController
     latitude = params[:latitude]
     longitude = params[:longitude]
     uri = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=#{longitude},#{latitude}&access_token=#{ENV['MAPBOX_API']}")
+    museums_hash = parse_uri(uri)
+    render json: museums_hash
+  end
+
+  private
+
+  def parse_uri(uri)
+    museums_hash = {}
     api_response = Net::HTTP.get_response(uri)
-    respond_to do |format|
-      format.html { render json: api_response.body }
+    api_json = JSON.parse(api_response.body)
+    museums = api_json['features']
+    museums.each do |museum|
+      postal_code = museum['context'][0]['text'].to_sym
+      museum_name = museum['text']
+      museums_hash[postal_code].present? ? museums_hash[postal_code] << museum_name : museums_hash[postal_code] = [museum_name]
     end
-    raise
+    museums_hash
   end
 end
